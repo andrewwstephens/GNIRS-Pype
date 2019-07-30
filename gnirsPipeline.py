edab774813ec8c2b75ee9cf2e0ddb47355d70241
Viraja Khatu <viraja.khatu@gmail.com>
2019-07-30 09:23:23 -1000

    Jul. 20, 2019: updated gnirs.cfg and gnirsPipeline.py

--- gnirsPipeline.py
+++ gnirsPipeline.py
@@ -35,11 +35,13 @@ import gnirsSort
 import gnirsCheckCalibrations
 import gnirsBaselineCalibration
 import gnirsReduce
-#import gnirsTelluric
-#import gnirsFluxCalibrate
-#import gnirsCombineOrdersXD
-#import gnirsCalcSNRspectrum
-#import writeDataSheet
+import gnirsCombineSpectra2D
+import gnirsExtractSpectra1D
+import gnirsTelluric
+import gnirsFluxCalibrate
+import gnirsCombineOrdersXD
+import gnirsCalculateSpectrumSNR
+import writeDataSheet
 
 # Import custom pipeline setup Class.
 #from objectoriented.GetConfig import GetConfig  ## this is not adapted for GNIRS as of July 2019
@@ -59,12 +61,12 @@ def start(args):
     This script does two things. It:
         - gets data reduction parameters; either from an interactive input session (not adapted for GNIRS as of July 
           2019) or an input file, and
-        - launches appropriate scripts to do the work. It can call up to five scripts directly:
+        - launches appropriate scripts to do the work. It can call up to five scripts directly:             ---------- TBD??
                 1) gnirsGetData.py
                 2) gnirsSort.py
                 3) gnirsCheckCalibrations.py
                 4) gnirsBaselineCalibration.py
-                5) gnirsReduce.py
+                5) gnirsReduce.py (science and/or telluric)
     """
     log.configure('gnirs.log', filelevel='INFO', screenlevel=args.loglevel)
     logger = log.getLogger('gnirsPipeline')
@@ -104,7 +106,9 @@ def start(args):
             logger.info(option + " = " + config.get(section,option))
     logger.info("")
 
-    # Andy wonders why there are TWO config files: gnirs.cfg and defaultConfig.cfg
+    # Andy wonders why there are TWO config files: gnirs.cfg and defaultConfig.cfg              
+    # Viraja:  If the script gives the flexibility of how the user wishes to reduce the data, which I think gnirs.cfg \
+    # does offer, then we do not need a gnirsDefault.cfg
     
     # Load general configuration from gnirs.cfg.
     manualMode = config.getboolean('defaults','manualMode')
@@ -115,45 +119,54 @@ def start(args):
     calibrationReduction = config.getboolean('gnirsPipeline','calibrationReduction')
     telluricReduction = config.getboolean('gnirsPipeline','telluricReduction')
     scienceReduction = config.getboolean('gnirsPipeline','scienceReduction')
+    combineSpectra2D = config.getboolean('gnirsPipeline','combineSpectra2D')
+    extractSpectra1D = config.getboolean('gnirsPipeline','extractSpectra1D')
     telluricCorrection = config.getboolean('gnirsPipeline','telluricCorrection')
     fluxCalibration = config.getboolean('gnirsPipeline','fluxCalibration')
     combineOrdersXD = config.getboolean('gnirsPipeline','combineOrdersXD')
+    calculateSpectrumSNR = config.getboolean('gnirsPipeline','calculateSpectrumSNR')
     writeDataSheet = config.getboolean('gnirsPipeline','writeDataSheet')
 
-    ###########################################################################
-    ##                         SETUP COMPLETE                                ##
-    ##                      BEGIN DATA REDUCTION                             ##
-    ##                                                                       ##
-    ##   Six Main Steps:                                                     ##  
-    ##       1) Get raw data - gnirsData.py                                  ##
-    ##       2) Sort raw data - gnirsSort.py                                 ##
-    ##       3) Check raw calibrations - gnirsCheckCalibrations.py           ##
-    ##       4) Reduce baseline calibrations - gnirsBaselineCalibration.py   ##
-    ##       5) Reduce telluric observations - gnirsReduce.py                ##
-    ##       6) Reduce science observations - gnirsReduce.py                 ##
-    ##                                                                       ##
-    ###########################################################################
+    ######################################################################################
+    ##                         SETUP COMPLETE                                           ##
+    ##                      BEGIN DATA REDUCTION                                        ##
+    ##                                                                                  ##
+    ##   Twelve Steps:                                                                  ##  
+    ##       1) Get raw data - gnirsData.py                                             ##
+    ##       2) Sort raw data - gnirsSort.py                                            ##
+    ##       3) Check raw calibrations - gnirsCheckCalibrations.py                      ##
+    ##       4) Reduce baseline calibrations - gnirsBaselineCalibration.py              ##
+    ##       5) Reduce observations (science and/or telluric) - gnirsReduce.py          ##
+    ##       6) Combine 2D spectra - gnirsCombine2Dspectra.py                           ##
+    ##       7) Extract 1D spectra - gnirsExtract1Dspectra.py                           ##
+    ##       8) Perform telluric correction - gnirsTelluric.py                          ##
+    ##       9) Perform flux calibration - gnirsFluxCalibrate.py                        ##
+    ##      10) Combine XD Orders - gnirsCombineOrdersXD.py                             ##
+    ##      11) Calculate the SNR spectrum - gnirsCalculateSpectrumSNR.py               ##
+    ##      12) Write data sheet - gnirsWriteDataSheet.py                               ##
+    ##                                                                                  ##
+    ######################################################################################
 
     ###########################################################################
-    ##                      STEP 1: Get raw data.                            ##
+    ##                      STEP 1: Get raw data                             ##
     ###########################################################################
     
     if getData:
         if manualMode:
-            a = raw_input('About to enter gnirsGetData.')
+            a = raw_input('About to enter gnirsGetData to locate the raw data directory or download data from the Gemini public archive.')
         gnirsGetData.start(args.config)
     
     ###########################################################################
-    ##                        STEP 2: Sort raw data.                         ##
+    ##                        STEP 2: Sort raw data                          ##
     ###########################################################################
 
     if sort:
         if manualMode:
-            a = raw_input('About to enter gnirsSort.')
+            a = raw_input('About to enter gnirsSort to sort observation types, and copy files and lists into appropriate directories for reduction.')
         gnirsSort.start(args.config)
 
     ###########################################################################
-    ##                  STEP 3: Check raw calibration data.                  ##
+    ##                  STEP 3: Check raw calibration data                   ##
     ###########################################################################
 
     if checkCalibrations:
@@ -162,62 +175,105 @@ def start(args):
         gnirsCheckCalibrations.start(args.config)
 
     ###########################################################################
-    ##                STEP 4: Reduce baseline calibrations.                  ##
+    ##                STEP 4: Reduce baseline calibrations                   ##
     ###########################################################################
     
     if calibrationReduction:
         if manualMode:
-            a = raw_input('About to enter gnirsBaselineCalibration.')
+            a = raw_input('About to enter gnirsBaselineCalibration to reduce baseline calibrations.')
         gnirsBaselineCalibration.start(args.config)
 
     ###########################################################################
-    ##                STEP 5: Reduce telluric observations.                  ##
-    ###########################################################################
-
-    if telluricReduction:
-        if manualMode:
-            a = raw_input('About to enter gnirsReduce to reduce Tellurics.')
-        gnirsReduce.start('Telluric')
-
-    ###########################################################################
-    ##                 STEP 6: Reduce science observations.                  ##
+    ##         STEP 5: Reduce observations (telluric and/or science)         ##
     ###########################################################################
 
     if scienceReduction:
         if manualMode:
-            a = raw_input('About to enter gnirsReduce to reduce science.')
-        gnirsReduce.start('Science')
+            a = raw_input('About to enter gnirsReduce to reduce science observations.')
+        gnirsReduce.start(args.config)
+
+    if telluricReduction:
+        if manualMode:
+            a = raw_input('About to enter gnirsReduce to reduce telluric observations.')
+        gnirsReduce.start(args.config)
+
+    ###########################################################################
+    ##                      STEP 6: Combine 2D spectra                       ##
+    ###########################################################################
+
+    if combineSpectra2D:
+        if manualMode:
+            a = raw_input('About to enter gnirsCombineSpectra2D to combine different spectral orders into a single 2D spectrum.')
+        gnirsCombineSpectra2D.start(args.config)
+
+    ###########################################################################
+    ##                      STEP 7: Extract 1D spectra                       ##
+    ###########################################################################
+
+    if extractSpectra1D:
+        if manualMode:
+            a = raw_input('About to enter gnirsExtractSpectra1D to extract different spectral orders into a individual 1D spectra.')
+        gnirsExtractSpectra1D.start(args.config)
+
+    ###########################################################################
+    ##                   STEP 8: Perform telluric correction                 ##
+    ###########################################################################
+
     if telluricCorrection:
         if manualMode:
-            a = raw_input('About to enter gnirsTelluric to make and create telluric corrected spectra.')
-        gnirsTelluric.run()
+            a = raw_input('About to enter gnirsTelluric to remove telluric absorption features from the extracted science spectra.')
+        gnirsTelluricCorrection.start(args.config)
+
+    ###########################################################################
+    ##                    STEP 9: Perform flux calibration                  ##
+    ###########################################################################
 
     if fluxCalibration:
         if manualMode:
-            a = raw_input('About to enter gnirsFluxCalibrate to make and create flux calibrated and telluric corrected '
-                          'spectra.')
-        gnirsFluxCalibrate.run()
+            a = raw_input('About to enter gnirsFluxCalibrate to create flux calibrated science spectra.')
+        gnirsFluxCalibrate.start(args.config)
 
-    if combineOrders:
+    ###########################################################################
+    ##                       STEP 10: Combine XD Orders                      ##
+    ###########################################################################
+
+    if combineOrdersXD:
         if manualMode:
-            a = raw_input('About to enter gnirsMerge to combine spectra from different orders into a single spectrum.')
-        combineOrders.run()
+            a = raw_input('About to enter gnirsCombineOrdersXD to combine diferent spectral orders.')
+        combineOrdersXD.start(args.config)
+    
+    ###########################################################################
+    ##                    STEP 11: Calculate SNR spectrum                    ##
+    ###########################################################################
+
+    if calculateSpectrumSNR:
+        if manualMode:
+            a = raw_input('About to enter gnirsCalculateSpectrumSNR to calculate the SNR spectrum.')
+        calculateSpectrumSNR.start(args.config)
+    
+    ###########################################################################
+    ##                        STEP 12: Write Data Sheet                      ##
+    ###########################################################################
+
+    if writeDataSheet:
+        if manualMode:
+            a = raw_input('About to enter gnirsWriteDataSheet to write a data sheet of results from GNIRS data reduction.')
+        writeDataSheet.start(args.config)
 
     ###########################################################################
     ##                    Data Reduction Complete!                           ##
     ##                  Good luck with your science!                         ##
     ###########################################################################
 
-    logger.info('\n###########################################################')
+    logger.info('###########################################################')
     logger.info('#                                                         #')
     logger.info('#               DATA REDUCTION COMPLETE                   #')
     logger.info('#             Good luck with your science!                #')
     logger.info('# Check out http://nifty4gemini.readthedocs.io/en/latest/ #')
     logger.info('#           For docs, tutorials and examples.             #')
     logger.info('#                                                         #')
-    logger.info('###########################################################')
-
-    '''
+    logger.info('###########################################################\n')
+s
     return
 
 if __name__ == '__main__':
